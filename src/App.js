476915deac1f43d7fa0c652c044c8c938d518b03
Marvin Ricardo Toro <ingricardotoro@gmail.com>
2020-09-17 11:41:47 -0600

    limpieza inicial

--- src/App.js
+++ src/App.js
@@ -8,15 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SISTEMA DE INVENTARIO Y PUNTO DE VENTA
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          
         >
-          Learn React
+          DATAPLUS OPEN SYSTEM
         </a>
       </header>
     </div>
