8e0271b4fa667e3849da052b72918072c6ece7c8
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-10-01 09:19:01 -0600

    links of users

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import  {Link} from 'react-router-dom';
+import  {Link} from 'react-router-dom';
 
 import userImg from './user_default.png'
 
@@ -9,7 +9,7 @@ export default function Header(){
             <nav className="pcoded-navbar menupos-fixed">
                 <div className="navbar-wrapper">
                     <div className="navbar-content scroll-div">
-                        <div className="">
+                        <div>
                             <div className="main-menu-header">
                                 <img src={userImg} alt="User default" className="img-radius"/>
                                 <div className="user-details">
@@ -17,8 +17,28 @@ export default function Header(){
                                     <div id="more-details"><small>Cargo</small></div>
                                 </div>
                             </div>
-                            
                         </div>
+                        <ul className="nav pcoded-inner-navbar">
+                            <li className="nav-item pcoded-menu-caption"><label htmlFor="">Usuarios</label></li>
+                            <li className="nav-item">
+                                <Link className="nav-link pcoded-hasmenu" to="/panel-admin">
+                                    <span className="pcoded-micon"><i className="ti-user"></i></span>
+                                    <span className="pcoded-mtext">Empleados</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link pcoded-hasmenu" to="/panel-admin">
+                                    <span className="pcoded-micon"><i className="ti-user"></i></span>
+                                    <span className="pcoded-mtext">Clientes</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link pcoded-hasmenu" to="/panel-admin">
+                                    <span className="pcoded-micon"><i className="ti-truck"></i></span>
+                                    <span className="pcoded-mtext">Proveedores</span>
+                                </Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -9,36 +9,31 @@ export default function Navbar(){
         <header className="navbar pcoded-header navbar-expand-lg navbar-light headerpos-fixed">
             <div className="m-header">
                 <div className="mobile-menu" id="mobile-collapse"><span></span></div>
-                <Link to="/" className="b-brand">
+                <Link to="/panel-admin" className="b-brand">
                     <img src={logo} alt="logo" className="logo"/>
                 </Link>
-                <Link to="" className="mob-toggler">
-                    <i className="feather icon-more-vertical"></i>
+                <Link to="/panel-admin" className="mob-toggler">
+                    <i className="ti-more-alt"></i>
                 </Link>
             </div>
             <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <div className="dropdown-menu dropdown-menu-right profile-notification">
-                            <div className="pro-head">
-                                <img src={userImg} alt="User-default" className="img-radius"/>
-                            QUE PEDO QUE PEDO
+                <ul className="navbar-nav ml-auto">
+                    <li>
+                        <div className="dropdown drp-user">
+                            <Link className="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                <i className="ti-user"></i>
+                            </Link>
+                            <div className="dropdown-menu dropdown-menu-right profile-notification">
+                                <div className="pro-head">
+                                    <img src={userImg} alt="user-default"/>
+                                    <span>Nombre</span>
+                                    <Link className="dud-logout" title="Logout"><i className="ti-lock"></i></Link>
+                                </div>
+                                <ul className="pro-body">
+                                    <li><Link className="dropdown-item"><i className="ti-user"></i>Perfil</Link></li>
+                                    <li><Link className="dropdown-item"><i className="ti-lock"></i>Cerrar Sesion</Link></li>
+                                </ul>
                             </div>
-                            <ul className="pro-body">
-                                <li>
-                                    <Link to="" className="dropdown-item">
-                                        <i className="fas fa-circle"></i>
-                                        Perfil
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="" className="dropdown-item">
-                                        <i className="fas fa-circle"></i>
-                                        Cerrar Sesion
-                                    </Link>
-                                </li>
-                                
-                            </ul>
                         </div>
                     </li>
                 </ul>
