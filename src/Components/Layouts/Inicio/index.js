a8eb54df76309ae547f2f56cc5aa05a1836d6337
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-10-26 16:04:52 -0600

    automatic component inicio

--- src/Components/Layouts/Inicio/index.js
+++ src/Components/Layouts/Inicio/index.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react';
 
-import CardGrafica from './CardGrafica'
+import empleadoContext from '../../../Context/empleados/empleadoContext';
+import clienteContext from '../../../Context/clientes/clienteContext';
+import proveedorContext from '../../../Context/proveedores/proveedorContext';
+
+import CardGrafica from './CardGrafica';
 
 export default function Inicio(){
+
+    const EmpleadoContext = useContext(empleadoContext);
+    const { empleados } = EmpleadoContext;
+
+    const ClienteContext = useContext(clienteContext);
+    const { clientes } = ClienteContext;
+
+    const ProveedorContext = useContext(proveedorContext);
+    const { proveedores } = ProveedorContext;
+
     return(
         <>
             <div className="row">
-                <CardGrafica index={1} nombre={'Clientes'} cantidad={6780}/>
-                <CardGrafica index={2} nombre={'Empleados'} cantidad={7451}/>
-                <CardGrafica index={3} nombre={'Proveedores'} cantidad={5628}/>
+                <CardGrafica index={1} nombre={'Clientes'} cantidad={clientes.length}/>
+                <CardGrafica index={2} nombre={'Empleados'} cantidad={empleados.length}/>
+                <CardGrafica index={3} nombre={'Proveedores'} cantidad={proveedores.length}/>
             </div>
         </>
     )
